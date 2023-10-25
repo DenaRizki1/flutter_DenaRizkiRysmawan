7927024c1fdda31e3284344d28a5b365ea9ce66e
Denarizki <86529218+DenaRizki1@users.noreply.github.com>
2023-10-25 13:08:46 +0700

    Open Ai

---
+++ 24_Open Ai/praktikum/openai/lib/env/env.g.dart
@@ -0,0 +1,12 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+class _Env {
+  static const String apiKey =
+      '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';
+}
